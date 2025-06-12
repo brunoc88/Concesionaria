bde70330804f2a0664204d301c92c6188f77ccb7
brunoc88 <brunocerutti88@gmail.com>
2025-06-12 11:21:12 -0300

    installing dependencies & config

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send('Hola!')
+})
+
+const PORT = 3000
+
+app.listen(PORT, ()=>{
+    console.log(`Escuchando puento:: 3000`)
+})
